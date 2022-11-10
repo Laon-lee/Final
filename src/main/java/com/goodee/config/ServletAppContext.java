96e27eea873e0cd5cc7b0949a7e021b1e2589f8d
장인우 <@192.168.12.38>
2022-11-10 09:37:21 +0900

    final 경로 타기

--- src/main/java/com/goodee/config/ServletAppContext.java
+++ src/main/java/com/goodee/config/ServletAppContext.java
@@ -68,7 +68,7 @@ public class ServletAppContext implements WebMvcConfigurer{
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		// TODO Auto-generated method stub
 		WebMvcConfigurer.super.addResourceHandlers(registry);
-		registry.addResourceHandler("/**").addResourceLocations("/resources/");
+		registry.addResourceHandler("/final/**").addResourceLocations("/resources/");
 		registry.addResourceHandler("/upload/**").addResourceLocations("file:///C:/finalprojectimage/");
 	}
 	
